233202a13af881a95efccd49e95b9e2428417d6e
Leon Björklund <leon.bjorklund@gmail.com>
2022-11-27 13:54:33 +0100

    changed overall design and added button functionality

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 /** nextStep takes two HTML-elements and hides one of them, and shows another.
- * @param {Element} currentScene - element that hides
- * @param {Element} nextScene - elementet that shows */
- function loadNextScene(currentScene, nextScene) {
-    currentScene.style.display = "none";
-    nextScene.style.display = "block";
-  }
+* @param {Element} currentScene - element that hides
+* @param {Element} nextScene - elementet that shows */
+function loadNextScene(currentScene, nextScene) {
+  currentScene.style.display = "none";
+  nextScene.style.display = "block";
+}
   
-const mainPage = document.getElementById("main-canvas");
-const icons = document.getElementById("icons");
+// const mainPage = document.getElementById("main-canvas");
+const mainPage = document.getElementById("main-page");
 
-const projectIcon = document.getElementById("projects");
+
 const projectPage = document.getElementById("project-page");
+const resumePage = document.getElementById("resume-page");
+const contactPage = document.getElementById("contact-page");
 
-projectIcon.addEventListener("click", function() {
-    mainPage.style.display = "none";
-    icons.style.display = "none";
-    projectPage.style.display = "block";
+const projectsButton = document.getElementsByClassName("projects-button")[0];
+const resumeButton = document.getElementsByClassName("resume-button")[0];
+const contactButton = document.getElementsByClassName("contact-button")[0];
+
+
+const homeButton = document.getElementsByClassName("nav-item")[0];
+
+homeButton.addEventListener('click', event => {
+  loadNextScene(projectPage, mainPage)
+  resumePage.style.display = "none";
+  contactPage.style.display = "none";
 })
 
+projectsButton.addEventListener('click', event => {
+  loadNextScene(mainPage, projectPage)
+})
+
+resumeButton.addEventListener('click', event => {
+  loadNextScene(mainPage, resumePage)
+})
+
+contactButton.addEventListener('click', event => {
+  loadNextScene(mainPage, contactPage)
+})
