7b5e90a5433d6677fdf161a74378c60de3d85ff3
Leon Björklund <leon.bjorklund@gmail.com>
2022-11-29 19:25:23 +0100

    some updates

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const mainPage = document.getElementById("main-page");
-
-
-const projectPage = document.getElementById("project-page");
-const resumePage = document.getElementById("resume-page");
-const contactPage = document.getElementById("contact-page");
-
-const projectsButton = document.getElementsByClassName("projects-button")[0];
-const resumeButton = document.getElementsByClassName("resume-button")[0];
-const contactButton = document.getElementsByClassName("contact-button")[0];
-
-const homeButton = document.getElementsByClassName("nav-item")[0];
-
+Gifffer();
